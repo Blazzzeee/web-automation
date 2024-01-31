9340fad639f0bd7dbd62fe79231cbf2903235866
Blazzzeee <148184733+Blazzzeee@users.noreply.github.com>
2024-01-31 21:58:13 +0530

    Add files via upload

--- fox.py
+++ fox.py
@@ -5,36 +5,38 @@ from selenium.webdriver.firefox.service import Service
 
 from selenium.webdriver.firefox.options import Options
 
-# from functools import cached_property
-from pyscript import document 
+from functools import cache 
+#from webdriver_manager.firefox import GeckoDriverManager  # Note "GeckoDriverManager"
 
 class fox:
+    
     def __init__(self):
-        from webdriver_manager.firefox import GeckoDriverManager  # Note "GeckoDriverManager"
+        self.instance=False
+    def launch(self):
         options=Options()
         options.add_argument("--headless")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--disable-gpu")
-        driver = webdriver.Firefox(options=options,service=Service(GeckoDriverManager().install()))
+        driver = webdriver.Firefox(options=options)#,service=Service(GeckoDriverManager().install()))
         driver.get("http://127.0.0.1:5500/scarp-1.html")
         self.driver=driver
-    # @cached_property
-    def button(self):
+        print("Window created")
+        self.instance = True
+        
+# =============================================================================
+#     @cache
+# =============================================================================
+    def login(self):
         elem=self.driver.find_element(By.ID,'username')
         elem2=self.driver.find_element(By.ID,'password')
         elem3=self.driver.find_element(By.ID,'loginbutton')
-        #elem.clear()
-        #elem2.clear()
+        elem.clear()
+        elem2.clear()
         elem.send_keys("")
         elem2.send_keys("")
         elem3.click()
-        print("Success")
-
-if __name__ == "__main__":
-    login=fox()
-
-
-
+        print("Logged-IN")
+        
 
 
 
